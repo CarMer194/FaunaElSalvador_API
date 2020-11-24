7c3b846a964e0e2b197ae85d425eb4574007cece
CarMer194 <00060313@uca.edu.sv>
2020-11-24 14:50:43 -0600

    agregando static files

--- FaunaElSalvador_API/settings.py
+++ FaunaElSalvador_API/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'API.apps.ApiConfig',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
--- FaunaElSalvador_API/urls.py
+++ FaunaElSalvador_API/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from API import views
+
+router = routers.DefaultRouter()
+router.register(r'grupoanimal', views.GrupoAnimalView)
+router.register(r'familiaanimal',views.FamiliaAnimalView)
+router.register(r'especieanimal',views.EspecieAnimalView)
+router.register(r'animal',views.AnimalView)
+router.register(r'expertos',views.ExpertoView)
+router.register(r'avistamientos',views.AvistamientoView)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
